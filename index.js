7bf69d57bd3c9ae1b57ce911fbc689bea58bd26e
root <root@DESKTOP-1IHRCNC>
2023-03-23 09:49:01 -0700

    Curse the forks

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+
+function superbowlWin (record) {
+    const ringYear = record.find(obj => obj.result === 'W');
+    const bummerYear = record.find(obj => obj.result === "N/A");
+    const seanPaytonYear = record.find(obj => obj.result === "L");
+    if (ringYear) {
+        return ringYear.year;
+    } else {
+        if (bummerYear) {
+            return undefined;
+        }   else {
+            if (seanPaytonYear) {
+                return seanPaytonYear.year;
+            }
+        }
+    }
+}
